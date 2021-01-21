54fb691ecd455f0c812005649198988ea0e75886
borgepl <pdeborger@gmail.com>
2021-01-21 14:56:36 +0100

    ListView, Adapter and ArrayList * implement ChatListAdapter

---
+++ app/src/main/java/com/deborger/myflashchat/ChatListAdapter.java
@@ -0,0 +1,54 @@
+package com.deborger.myflashchat;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class ChatListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+
+    public ChatListAdapter(Activity activity, DatabaseReference databaseReference, String displayName) {
+        mActivity = activity;
+        mDatabaseReference = databaseReference.child("messages");
+        mDisplayName = displayName;
+        mSnapshotList = new ArrayList<>();
+    }
+
+    static class ViewHolder {
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
